ff26270fe6100d9ec3a852c2b71def4cfdcdf4b5
Sam <samuel.fitz@btinternet.com>
2023-01-12 13:50:11 +0000

    feat: create vite project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import "./navbar.css";
+
+type Props = {};
+
+function Navbar({}: Props) {
+  return (
+    <header>
+      <a href="/">Haikupad</a>
+    </header>
+  );
+}
+
+export default Navbar;

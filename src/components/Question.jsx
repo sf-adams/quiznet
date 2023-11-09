70779be437d7111ae88da0ae7e6185b805b57851
Sam <samuel.fitz@btinternet.com>
2023-11-09 10:40:57 +0000

    feat: randomise answers and add conditional colours

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,9 +4,28 @@ import "../styles/question.css";
 export default function Question({ question }) {
   const answers = [...question.incorrect_answers, question.correct_answer];
 
-  console.log(answers);
-  const answerElements = answers.map((answer, index) => (
-    <li className="question__item" key={index}>{decode(answer)}</li>
+  // Fisher-Yates shuffle used to randomise the array
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const randomAnswers = shuffle(answers.slice());
+
+  const answerElements = randomAnswers.map((answer, index) => (
+    <li
+      className="question__item"
+      style={{
+        backgroundColor:
+          answer === question.correct_answer ? "#94D7A2" : "#F8BCBC",
+      }}
+      key={index}
+    >
+      {decode(answer)}
+    </li>
   ));
 
   return (
